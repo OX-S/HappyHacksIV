761f222d86711c8c8f2782312f75a147b04c6876
OX-S <finn.kliewer@gmail.com>
2024-01-07 00:20:36 -0500

    added login functionality

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
-from views import views
 from config import secret_key
+from views import views
+
 
 app = Flask(__name__)
 app.secret_key = secret_key
 app.register_blueprint(views, url_prefix="/")
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True, port=8080)
--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, url_for, render_template, request, redirect
+from flask import Blueprint, url_for, render_template, request, redirect, flash
 
 views = Blueprint(__name__, "views")
 
@@ -8,14 +8,29 @@ def landing():
     # return "hello"
     return redirect(url_for('views.login_page'))
 
+@views.route("/home")
+def home():
+    return render_template("home.html")
 
-@views.route("/login")
+
+@views.route("/login", methods=['GET', 'POST'])
 def login_page():
-    return render_template("login.html")
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
 
-@views.route("/register")
+        if username == 'admin' and password == '1234':
+            flash('Login successful!', 'success')
+            return redirect(url_for('views.home'))
+        else:
+            flash('Login unsuccessful. Please check your username and password.', 'danger')
+
+    return render_template('login.html')
+
+
+@views.route("/register", methods=['GET', 'POST'])
 def register_page():
-    return render_template("register.html")
+    return render_template('register.html')
 
 
 @views.route("/emmaisadoofus")
