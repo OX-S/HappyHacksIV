76b309e7c333146c9ccae87e6aed9beb68f733ad
OX-S <finn.kliewer@gmail.com>
2024-01-06 22:29:52 -0500

    added login page

--- views.py
+++ views.py
@@ -1,9 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint, url_for, render_template, request, redirect
 
 views = Blueprint(__name__, "views")
 
 
 @views.route("/")
 def landing():
-    return "hello"
-    # return redirect(url_for('views.home'))
+    # return "hello"
+    return redirect(url_for('views.login_page'))
+
+
+@views.route("/login")
+def login_page():
+    return render_template("login.html")
+
+
+@views.route("/emmaisadoofus")
+def emmaisadoofus():
+    return "emma is a DOOFUS"
