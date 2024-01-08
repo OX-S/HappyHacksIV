7ee27c3dca36e02af8ccfc72848614a53265e665
OX-S <finn.kliewer@gmail.com>
2024-01-07 21:56:47 -0500

    backup (might be final)

--- views.py
+++ views.py
@@ -5,14 +5,33 @@ from secrets import compare_digest
 views = Blueprint(__name__, "views")
 
 schools = [
-    School(1, 'School A', 40.123, -74.456, 10000, 2750, 'Description for School A', "images/School_A.jpg"),
-    School(2, 'School B', 40.456, -74.789, 15000, 7430, 'Description for School B', "/images/School_A.jpg"),
-    School(3, 'School C', 40.456, -74.789, 7000, 1714, 'This is a longer description for School C to showcase the '
-                                                       'truncation of the description in the CSS after 100 chars. '
-                                                       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, '
-                                                       'sed do eiusmod tempor incididunt ut labore et dolore magna '
-                                                       'aliqua. Ut enim ad minim veniam, quis nostrud exercitation '
-                                                       'ullamco laboris',
+    School(1, 'School A', 40.123, -74.456, 10000, 2750, "Welcome to School A, a resilient community striving for "
+                                                        "excellence despite the challenges we face. Our institution, "
+                                                        "with a rich history, grapples with economic and racial "
+                                                        "disparities that have left us in need of essential "
+                                                        "resources. Your support is crucial in helping us overcome "
+                                                        "these challenges. We urgently require basic supplies such as "
+                                                        "notebooks, pencils, and art materials to create an "
+                                                        "environment where every learner can flourish. Your generous "
+                                                        "donation will make a lasting impact and contribute to the "
+                                                        "transformation of our educational journey.",
+           "images/School_A.jpg"),
+    School(2, 'School B', 40.456, -74.789, 15000, 7430, "Welcome to School B, where the pursuit of knowledge knows no "
+                                                        "bounds. As students in an historically underfunded school, "
+                                                        "we face hurdles that many can't imagine. The lack of "
+                                                        "resources hampers our learning experience, but your support "
+                                                        "can change that. We urgently need textbooks, calculators, "
+                                                        "and science equipment to enhance our education. Your "
+                                                        "donation will empower us to overcome challenges and reach "
+                                                        "our full potential.", "/images/School_A.jpg"),
+    School(3, 'School C', 40.456, -74.789, 7000, 1714, "Greetings from School C, where resilience defines us despite "
+                                                       "the obstacles we face. Economic and racial disparities have "
+                                                       "left us with limited resources, but our determination to "
+                                                       "learn remains unwavering. Your contribution can help bridge "
+                                                       "the gap. We are in need of basic classroom supplies, "
+                                                       "including backpacks, lunchboxes, and uniforms. Your generous "
+                                                       "support will provide us with the essentials we need to focus "
+                                                       "on our education and build a brighter future.",
            "/images/School_A.jpg"),
 
 ]
